e593ecd52524e830dcfbe08303e134f87af741ef
11249a239-dot <11249a239@kanchiuniv.ac.in>
2025-11-20 20:42:49 +0530

    Create infix to post fix.c

---
+++ infix to post fix.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAX 100
+
+char stack[MAX];
+int top = -1;
+
+void push(char x) {
+    stack[++top] = x;
+}
+
+char pop() {
+    return stack[top--];
+}
+
+int precedence(char x) {
+    if (x == '^')
+        return 3;
+    if (x == '*' || x == '/')
+        return 2;
+    if (x == '+' || x == '-')
+        return 1;
+    return 0;
+}
+
+int main() {
+    char infix[MAX], postfix[MAX];
+    int i, j = 0;
+
+    printf("Enter infix expression: ");
+    scanf("%s", infix);
+
+    for (i = 0; i < strlen(infix); i++) {
+        char ch = infix[i];
+
+        if (isalnum(ch)) {
+            postfix[j++] = ch;   // operand → output
+        }
+        else if (ch == '(') {
+            push(ch);
+        }
+        else if (ch == ')') {
+            while (top != -1 && stack[top] != '(')
+                postfix[j++] = pop();
+            pop(); // remove '('
+        }
+        else {
+            while (top != -1 && precedence(stack[top]) >= precedence(ch))
+                postfix[j++] = pop();
+            push(ch);
+        }
+    }
+
+    // pop all remaining operators
+    while (top != -1)
+        postfix[j++] = pop();
+
+    postfix[j] = '\0';
+
+    printf("Postfix expression: %s\n", postfix);
+
+    return 0;
+}
